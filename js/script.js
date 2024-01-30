a625631534f9f18d2439109907e332b654c22018
Sergiu Stancioiu <s.sergiu96@gmail.com>
2024-01-30 23:25:18 +0200

    I worked on the design of Cart

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
 const closeMenu = document.querySelector(".close-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const cart = document.querySelector(".cart");
+const cartContainer = document.querySelector(".cart-wrapper");
 
 hamburger.addEventListener("click", toggleMobileMenu);
 closeMenu.addEventListener("click", toggleMobileMenu);
+cart.addEventListener("click", toggleCart);
 
 function toggleMobileMenu() {
-  console.log("toggle mobile menu");
   mobileMenu.classList.toggle("active");
+}
+
+function toggleCart() {
+  cartContainer.classList.toggle("hidden");
 }
