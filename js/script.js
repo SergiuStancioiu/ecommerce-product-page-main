b66c7bfefc4ee5d5340e312f24004b7a16d66456
Sergiu Stancioiu <s.sergiu96@gmail.com>
2024-02-02 20:25:20 +0200

    I fixed some bugs with mobile menu

--- js/script.js
+++ js/script.js
@@ -2,22 +2,19 @@ const hamburger = document.querySelector(".hamburger");
 const closeMenu = document.querySelector(".close-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const cart = document.querySelector(".cart");
-const cartContainer = document.querySelector(".cart-wrapper");
 const minusBtn = document.querySelector(".minus");
-const qty = document.querySelector(".qty");
 const plusBtn = document.querySelector(".plus");
-const productNumber = document.querySelector(".prod-nr");
-const cartProductQty = document.querySelector(".prod-qty");
-const productsTotalPrice = document.querySelector(".prod-total-price");
 const addToCartBtn = document.querySelector(".add-to-cart-wrapper");
-const cartProductNumber = document.querySelector(".cart-product-number");
 const deleteProdFromCart = document.querySelector(".delete-prod");
-const noProductsInCart = document.querySelector(".no-products");
-const productCartWrapper = document.querySelector(".product-cart-wrapper");
-const productPrice = document.querySelector(".prod-price");
+const overlay = document.querySelector(".overlay");
+const sliderRightBtn = document.querySelector(".arrow-right");
+const sliderLeftBtn = document.querySelector(".arrow-left");
+
+document.addEventListener("DOMContentLoaded", checkCartQtyIsZero);
 
 hamburger.addEventListener("click", toggleMobileMenu);
 closeMenu.addEventListener("click", toggleMobileMenu);
+overlay.addEventListener("click", toggleMobileMenu);
 
 cart.addEventListener("click", toggleCart);
 
@@ -26,25 +23,32 @@ minusBtn.addEventListener("click", decrementQty);
 addToCartBtn.addEventListener("click", addProductsToCart);
 deleteProdFromCart.addEventListener("click", deleteProduct);
 
+sliderRightBtn.addEventListener("click", sliderImage);
+sliderLeftBtn.addEventListener("click", sliderImage);
+
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("active");
 }
 
 function toggleCart() {
+  const cartContainer = document.querySelector(".cart-wrapper");
+
   cartContainer.classList.toggle("hidden");
 }
 
 function incrementQty() {
+  const qty = document.querySelector(".qty");
+
   let qtyNumber = parseInt(qty.textContent);
 
-  if (qtyNumber >= 0) {
-    qtyNumber = qtyNumber + 1;
-  }
+  qtyNumber = qtyNumber + 1;
 
   qty.textContent = qtyNumber;
 }
 
 function decrementQty() {
+  const qty = document.querySelector(".qty");
+
   let qtyNumber = parseInt(qty.textContent);
 
   if (qtyNumber > 0) {
@@ -54,11 +58,25 @@ function decrementQty() {
   qty.textContent = qtyNumber;
 }
 
-if (parseInt(qty.textContent) === 0) {
-  cartProductNumber.classList.add("hidden");
+function checkCartQtyIsZero() {
+  const qty = document.querySelector(".qty");
+  const cartProductNumber = document.querySelector(".cart-product-number");
+
+  if (parseInt(qty.textContent) === 0) {
+    cartProductNumber.classList.add("hidden");
+  }
 }
 
 function addProductsToCart() {
+  const qty = document.querySelector(".qty");
+  const productNumber = document.querySelector(".prod-nr");
+  const cartProductQty = document.querySelector(".prod-qty");
+  const productsTotalPrice = document.querySelector(".prod-total-price");
+  const cartProductNumber = document.querySelector(".cart-product-number");
+  const noProductsInCart = document.querySelector(".no-products");
+  const productCartWrapper = document.querySelector(".product-cart-wrapper");
+  const productPrice = document.querySelector(".prod-price");
+
   let qtyNumber = parseInt(qty.textContent);
 
   if (qtyNumber > 0) {
@@ -79,10 +97,39 @@ function addProductsToCart() {
 }
 
 function deleteProduct() {
+  const qty = document.querySelector(".qty");
+  const cartProductNumber = document.querySelector(".cart-product-number");
+  const noProductsInCart = document.querySelector(".no-products");
+  const productCartWrapper = document.querySelector(".product-cart-wrapper");
+
   qty.textContent = 0;
 
   productCartWrapper.classList.add("hidden");
   cartProductNumber.classList.add("hidden");
 
   noProductsInCart.classList.remove("hidden");
+}
+
+function sliderImage() {
+  const sliderImg = document.querySelector(".slider-image");
+
+  // const imageArray = [
+  //   { id: 0, srcImage: "image-product-1.jpg", active: true },
+  //   { id: 1, srcImage: "image-product-2.jpg", active: false },
+  //   { id: 2, srcImage: "image-product-3.jpg", active: false },
+  //   { id: 3, srcImage: "image-product-4.jpg", active: false },
+  // ];
+
+  // let imgSrc = "";
+  // imageArray.forEach((image) => {
+  //   console.log(image);
+
+  //   imgSrc = image.srcImage;
+  // });
+
+  // console.log(imgSrc);
+
+  const currentImage = sliderImg.nextElementSibling.innerHTML;
+
+  console.log(currentImage);
 }
